a849a705ff154db73d290204a7c8080d67c8fa65
qian2 <yanlin.qian@student.tut.fi>
2017-05-09 17:21:25 +0300

    fire, explosion added.

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour
+{
+	public Transform explosionPrefab;
+	void OnCollisionEnter (Collision col)
+	{
+		//Destroy(col.gameObject);
+		//print(gameObject.name + " and " + col.collider.name);
+		ContactPoint contact = col.contacts[0];
+		Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		Vector3 pos = contact.point;
+		Instantiate(explosionPrefab, pos, rot);
+		//Destroy(gameObject);
+	}
+	void OnCollisionStay (Collision col){
+		
+	}
+	void OnCollisionExit (Collision col){
+	
+	}
+}
